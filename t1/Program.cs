0abfea686a4a4593d66283fd6326dbc5a323f3d0
Mikhail <goitw@outlook.com>
2023-02-03 19:57:26 +0100

    hw done

---
+++ t1/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] GenerateArray(int len=10)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-10, 10);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Count(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]> 0)
+        {
+        sum++;
+        } 
+    }
+    return sum;
+}
+
+int[] array = GenerateArray();
+PrintArray(array);
+System.Console.WriteLine(Count(array));
---
+++ t2/Program.cs
@@ -0,0 +1,15 @@
+﻿int Promt(string msg)
+{
+    System.Console.Write($"{msg}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+double k1 = Promt("Put k1");
+double k2 = Promt("Put k2");
+double b1 = Promt("Put b1");
+double b2 = Promt("Put b2");
+
+double x = (b2 - b1) / (-k2+k1);
+double y = (b2*k1 - b1*k2) / (- k2 + k1);
+
+System.Console.WriteLine($"[{x},{y}]");
